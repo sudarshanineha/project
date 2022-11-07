89e037b3ff533c653cfada7076c49d10a11dda5c
Neha <sakneha99@gmail.com>
2022-11-07 16:22:18 +0530

    first commit

---
+++ src/components/Edit.js
@@ -0,0 +1,59 @@
+import React,{useState,useEffect} from "react";
+import {Button,Form, FormControl} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Employees from './Employees';
+import {v4 as uuid} from "uuid";
+import {Link,useNavigate} from 'react-router-dom';
+
+function Edit(){
+    const[name,setName] = useState('');
+    const[age,setAge] = useState('');
+    const[id,setId] = useState('');
+
+    let history = useNavigate();
+
+    var index = Employees.map(function(e){
+        return e.id
+    }).indexOf(id);
+
+    const handleEdit = (e) =>{
+        e.preventDefault();
+
+       let a = Employees[index];
+       a.Name = name;
+       a.Age = age;
+
+        history("/");
+    }
+
+    useEffect(()=>{
+        setName(localStorage.getItem('Name'))
+        setAge(localStorage.getItem('Age'))
+        setId(localStorage.getItem('Id'))
+
+    },[])
+
+    return(
+        <id>
+            <Form className="d-grid gap-2" style={{margin:"15rem"}}>
+            <Form.Group className="mb-3" controlId="formName">
+                <FormControl type="text" placeholder="Enter Name" value={name} required onChange={(e) => setName(e.target.value)}>
+
+                </FormControl>
+
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formAge">
+                <FormControl type="text" placeholder="Enter Age" value={age} required onChange={(e) => setAge(e.target.value)}>
+
+                </FormControl>
+
+            </Form.Group>
+            <Button onClick={(e) => handleEdit(e)} type="submit">Update</Button>
+        
+        </Form>
+        </id>
+    )
+
+}
+
+export default Edit;
---
+++ src/components/Employees.js
@@ -0,0 +1,22 @@
+const Employees =[
+    {
+        id:"1",
+        Name:"test",
+        Age:"30"
+
+    },
+    {
+        id:"2",
+        Name:"test1",
+        Age:"20"
+
+    },
+    {
+        id:"3",
+        Name:"test2",
+        Age:"22"
+
+    }
+]
+
+export default Employees;
